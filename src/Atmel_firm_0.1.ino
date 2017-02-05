e0dd7c548110b72b06b655dd6030d18d20ef549a
JuanMiguel <juanmiguel@Synergy.local>
2017-02-05 18:22:05 -0300

    mas cambios y backup de /lib

--- src/Atmel_firm_0.1.ino
+++ src/Atmel_firm_0.1.ino
@@ -9,25 +9,9 @@
 #include <LiquidCrystal.h> // lcd de 16 x 2
 #include <DHT.h> // sensor de temp y humedad
 
-
-/**************************************************
-* C O N S T A N T E S - Sensores conectados al IM *
-*                                                 *
-* Comentar los sensores que no estén presentes    *
-**************************************************/
-#define DHT_TYPE           DHT21    // DHT 22  (AM2302)
-//#define DHT_TYPE           DHT22    // DHT 22  (AM2302)
-#define DHT_PIN_0          2        // pin al que va el cable de dato del sensor0 de temperatura
-//#define DHT_PIN_1          3      // pin al que va el cable de dato del sensor1 de temperatura
-#define LIGHT_SENSOR_PIN_0 A0       //PARA EL DE PEDRO
-//#define LIGHT_SENSOR_PIN_0 A2       //define el pin para el Photo-resistor
-//#define LIGHT_SENSOR_PIN_1 A0     //define el pin para el Photo-resistor
-//#define SOIL_PIN_0         A1       //define el pin para sensor de humedad de tierra
-//#define SOIL_PIN_1         A3     //define el pin para sensor de humedad de tierra
-
-/*******************************************
- * C O N S T A N T E S- Textos para el lcd *
- ******************************************/
+/********************************************
+ * C O N S T A N T E S - Textos para el lcd *
+ *******************************************/
 #define T_s0	  "Temp s0: "
 #define T_s1	  "Temp s1: "
 #define H_s0    " Hum s0: "
@@ -43,45 +27,64 @@
 /**************************************************
  * C O N S T A N T E S - Relativas al Dispositivo *
  *************************************************/
-//const String  dispositivo = "e4da3b7fbbce2345d7772b0674a318d5"; /*5*/  //nombre del dispositivo Importante cambiarlo por cada dispositivo
-const String  dispositivo = "TEST";
-const int     numeroSerie = 3;
+const String  dispositivo = "a87ff679a2f3e71d9181a67b7542122c"; /*4*/  //nombre del dispositivo Importante cambiarlo por cada dispositivo
+//const String  dispositivo = "TEST";
+const int     numeroSerie = 4;
 //tiempo entre envío a la DB
-//const long  intervalo = 360000; //constante de espera para mandar el GET
-const long intervalo = 60000; //constante de espera para mandar el GET mas corto para TEST
+const long  intervalo = 360000; //constante de espera para mandar el GET
+const long  intervaloSensores = 5000; //constante de espera para mandar el GET
+//const long intervalo = 60000; //constante de espera para mandar el GET mas corto para TEST
+
+/**************************************************
+* C O N S T A N T E S - Sensores conectados al IM *
+*                                                 *
+* Comentar los sensores que no estén presentes    *
+**************************************************/
+//#define DHT_TYPE           DHT21    // DHT 22  (AM2302)
+#define DHT_TYPE           DHT22    // DHT 22  (AM2302)
+#define DHT_PIN_0          2        // pin al que va el cable de dato del sensor0 de temperatura
+#define DHT_PIN_1          3      // pin al que va el cable de dato del sensor1 de temperatura
+//#define LIGHT_SENSOR_PIN_0 A0       //PARA EL DE PEDRO
+#define LIGHT_SENSOR_PIN_0 A2       //define el pin para el Photo-resistor
+#define LIGHT_SENSOR_PIN_1 A0     //define el pin para el Photo-resistor
+#define SOIL_PIN_0         A1       //define el pin para sensor de humedad de tierra
+//#define SOIL_PIN_1         A3     //define el pin para sensor de humedad de tierra
 
 /********************
 * V A R I A B L E S *
 ********************/
-unsigned long previousMillis = 0; //variable que va a guardar el valor de tiempo anterior para compararlo con el actual
+unsigned long previousMillisEnviaDatos = 0; //variable que va a guardar el valor de tiempo anterior para compararlo con el actual
+unsigned long previousMillisLeeSensores = 0; //variable que va a guardar el valor de tiempo anterior para compararlo con el actual
 unsigned long currentMillis; //varialble que guardará el valor de tiempo actual
 
 int     debug = 0;
 
-float   hum_0 = 999;  // humedad del sensor 0
-float   hum_1 = 999;  // humedad del sensor 1
-float   temp_0 = 999;  // temperatura del sensor 0
-float   temp_1 = 999;  // temperatura del sensor 0
 
-int     luz_0 = 999; //valor analógico del pin
-int     luz_1 = 999; //valor analógico del pin
-int     valorLuz_0 = 999; //valor mapeado
-int     valorLuz_1 = 999; //valor mapeado
+float hum_0         = 999;  // humedad del sensor 0
+float hum_1         = 999;  // humedad del sensor 1
+float temp_0        = 999;  // temperatura del sensor 0
+float temp_1        = 999;  // temperatura del sensor 0
 
-int     suelo_0 = 999; //valor analogico del pin
-int     suelo_1 = 999; //valor analogico del pin
-float   valorSuelo_0 = 999; //valor mapeado
-float   valorSuelo_1 = 999; //valor mapeado
+int luz_0           = 999; //valor analógico del pin
+int luz_1           = 999; //valor analógico del pin
+int valorLuz_0      = 999; //valor mapeado
+int valorLuz_1      = 999; //valor mapeado
 
-int     i = 0;
+int suelo_0         = 999; //valor analogico del pin
+int suelo_1         = 999; //valor analogico del pin
+float valorSuelo_0  = 999; //valor mapeado
+float valorSuelo_1  = 999; //valor mapeado
+int dbg             = 0; //
 
+int i               = 0;
 
-String stringDelSerial = "";         // a string to hold incoming data
+String stringDelSerial  = "";         // a string to hold incoming data
 
-boolean stringCompleta  = false;  // whether the string is complete
-boolean online          = false;  // bandera si esta ONLINE
-boolean exito           = false;  // bandera si la web devolvio exito
-
+boolean stringCompleta      = false;  // whether the string is complete
+boolean online              = false;  // bandera si esta ONLINE
+boolean exito               = true;  // bandera si la web devolvio exito
+boolean esperandoRespuesta  = false;
+boolean errorLecturaSensor  = false;
 /****************************************************
 * Construct de librerias del LCD y los sensores DHT *
 ****************************************************/
@@ -137,7 +140,7 @@ void loop()
     estaConectado();
     if ( i % 5 == 0)
     {
-      //leeSensores();
+      leeSensores();
       lcdSensores();
     }
   }
@@ -149,14 +152,11 @@ void loop()
       Serial.print("\n");
       Serial.println(F("loop - > if(online)"));
     }
-    //leeSensores();
-    lcdSensores();
+
+
     enviaDatos();
-    if( exito )
-    {
-      exito = false;
-      lcdExito();
-    }
+    leeSensores();
+    lcdSensores();
   }
 }
 
@@ -165,7 +165,7 @@ void loop()
  * F U N C I O N E S - Cosas que se usan en Setup y en Loop *
  ***********************************************************/
 
-// Genera un string a enviar
+// Genera el string a enviar
 void enviaDatos()
 {
   if (!online)
@@ -175,15 +175,20 @@ void enviaDatos()
 
   currentMillis = millis(); //asigna el valor de millis() a la variable currentMillis
 
-  if (previousMillis > currentMillis) // si previousMillis es mayor a currentMillis quiere decir que millis() volvio a cero porque se lleno entonces vuelvo tambien a cero previousMillis
+  if (previousMillisEnviaDatos > currentMillis) // si previousMillisEnviaDatos es mayor a currentMillis quiere decir que millis() volvio a cero porque se lleno entonces vuelvo tambien a cero previousMillisEnviaDatos
   {
-    previousMillis = 0;
+    previousMillisEnviaDatos = 0;
   }
 
-  if (currentMillis - previousMillis >= intervalo) //cuando la diferencia entre previousMillis y currentMillis es mayor o igual al intervalo se envian los datos
+  if ( ( currentMillis - previousMillisEnviaDatos >= intervalo ) || !exito )//cuando la diferencia entre previousMillisEnviaDatos y currentMillis es mayor o igual al intervalo se envian los datos
   {
+    if (!exito)
+    {
+      delay(1000);
+    }
     lcdEnviandoDatos();
-    previousMillis = currentMillis;
+    previousMillisEnviaDatos = currentMillis;
+
     String GET = "<?1a1dc91c907325c69271ddf0c944bc72";
     GET += "&disp=";
     GET += dispositivo;
@@ -204,105 +209,97 @@ void enviaDatos()
     GET += "&ht1=";
     GET += valorSuelo_1;
     GET += "&debug=";
-    GET += debug;
+    GET += dbg;
     GET += ">";
 
-    delay(1000);
+    //delay(500);
     Serial.print(GET);
-
-    /*if (Serial.find("EXITO"))
-    {
-      lcdExito();
-    }*/
+    esperandoRespuesta = true;
   }
 }
 
-// Pregunta al Wifi si esta conectado y busca en el buffer Serial CONECTADO_OK para pasar a true la variable online
+// Pregunta al Wifi si esta conectado
 void estaConectado ()
 {
   //pregunta al ESP si esta conectato
   delay(2000);
   Serial.print("[ESP_status]");
-  //Si llega un OK
-  /*if (Serial.find("CONECTADO_OK"))
-  {
-    online = true;
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("- ONLINE -    ");
-    delay(3000);
-    if (debug > 1)
-    {
-      Serial.println(F("DBG_esp8266_CONECTADO"));
-    }
-  }
-  else
-  {
-    online = false;
-    if (debug > 1)
-    {
-      Serial.println(F("esp8266 OFFLINE"));
-    }
-  }
-  */
+  esperandoRespuesta = true;
 }
 
 // lee los sensores y guarda los valores en sus variables.
 void leeSensores()
 {
-  #ifdef DHT_PIN_0
-    hum_0        = dht_0.readHumidity();
-    //delay(10);
-    temp_0       = dht_0.readTemperature();
-    //delay(10);
-    if (isnan(hum_0) || isnan(temp_0) )
+  currentMillis = millis(); //asigna el valor de millis() a la variable currentMillis
+
+  if ( previousMillisLeeSensores > currentMillis ) // si previousMillisEnviaDatos es mayor a currentMillis quiere decir que millis() volvio a cero porque se lleno entonces vuelvo tambien a cero previousMillisEnviaDatos
+  {
+    previousMillisLeeSensores = 0;
+  }
+
+  if ( ( currentMillis - previousMillisLeeSensores >= intervaloSensores ) || errorLecturaSensor )//cuando la diferencia entre previousMillisLeeSensores y currentMillis es mayor o igual al intervalo se envian los datos
+  {
+    if (errorLecturaSensor)
     {
       delay(3000);
+      errorLecturaSensor = false;
+    }
+    #ifdef DHT_PIN_0
       hum_0        = dht_0.readHumidity();
       //delay(10);
       temp_0       = dht_0.readTemperature();
       //delay(10);
-    }
-  #endif
+      if (isnan(hum_0) || isnan(temp_0) )
+      {
+        lcdErrorLeeSensores();
+        errorLecturaSensor = true;
+        //hum_0        = dht_0.readHumidity();
+        //delay(10);
+        //temp_0       = dht_0.readTemperature();
+        //delay(10);
+      }
+    #endif
 
-  #ifdef DHT_PIN_1
-    hum_1        = dht_1.readHumidity();
-    //delay(10);
-    temp_1       = dht_1.readTemperature();
-    //delay(10);
-    if (isnan(hum_0) || isnan(temp_0) )
-    {
-      delay(3000);
-      hum_1        = dht_0.readHumidity();
+    #ifdef DHT_PIN_1
+      hum_1        = dht_1.readHumidity();
       //delay(10);
-      temp_1       = dht_0.readTemperature();
+      temp_1       = dht_1.readTemperature();
       //delay(10);
-    }
-  #endif
+      if (isnan(hum_0) || isnan(temp_0) )
+      {
+        lcdErrorLeeSensores();
+        errorLecturaSensor = true;
+        //hum_1        = dht_0.readHumidity();
+        //delay(10);
+        //temp_1       = dht_0.readTemperature();
+        //delay(10);
+      }
+    #endif
 
-  #ifdef LIGHT_SENSOR_PIN_0
-    luz_0        = analogRead(LIGHT_SENSOR_PIN_0);
-    valorLuz_0   = map(luz_0,0,1024,0,100);
-    //delay(10);
-  #endif
+    #ifdef LIGHT_SENSOR_PIN_0
+      luz_0        = analogRead(LIGHT_SENSOR_PIN_0);
+      valorLuz_0   = map(luz_0,0,1024,0,100);
+      //delay(10);
+    #endif
 
-  #ifdef LIGHT_SENSOR_PIN_1
-    luz_1        = analogRead(LIGHT_SENSOR_PIN_1);
-    valorLuz_1   = map(luz_1,0,1024,0,100);
-    //delay(10);
-  #endif
+    #ifdef LIGHT_SENSOR_PIN_1
+      luz_1        = analogRead(LIGHT_SENSOR_PIN_1);
+      valorLuz_1   = map(luz_1,0,1024,0,100);
+      //delay(10);
+    #endif
 
-  #ifdef SOIL_PIN_0
-    suelo_0      = analogRead(SOIL_PIN_0);
-    valorSuelo_0 = map(suelo_0,0,1024,100,0);
-    //delay(10);
-  #endif
+    #ifdef SOIL_PIN_0
+      suelo_0      = analogRead(SOIL_PIN_0);
+      valorSuelo_0 = map(suelo_0,0,1024,100,0);
+      //delay(10);
+    #endif
 
-  #ifdef SOIL_PIN_1
-    suelo_1      = analogRead(SOIL_PIN_1);
-    valorSuelo_1 = map(suelo_1,0,1024,100,0);
-    //delay(10);
-  #endif
+    #ifdef SOIL_PIN_1
+      suelo_1      = analogRead(SOIL_PIN_1);
+      valorSuelo_1 = map(suelo_1,0,1024,100,0);
+      //delay(10);
+    #endif
+  }
 
   if (debug > 1)
   {
@@ -328,7 +325,7 @@ void leeSensores()
 /******************************/
 /* F U N C I O N E S   L C D  */
 /******************************/
-// Funcion que escribe dos renglones en la pantalla segun los parametros.
+// Funcion que escribe dos renglones en la pantalla segun los sensores presentes.
 void lcdSensores( )
 {
   if( debug > 1)
@@ -337,8 +334,11 @@ void lcdSensores( )
     Serial.print("\n");
     Serial.println(F("lcdSensores delay 3000ms por gurpo de sensor"));
   }
-  //i = 0;
 
+  if (esperandoRespuesta || errorLecturaSensor)
+  {
+    return;
+  }
   #ifdef DHT_PIN_0
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -392,10 +392,9 @@ void lcdSensores( )
         #endif
     delay(3000);
   #endif
-  //delay(500);
 }
 
-// escribe offline en el LCD
+// escribe offline
 void lcdOFFLINE()
 {
   if( debug > 1)
@@ -411,17 +410,17 @@ void lcdOFFLINE()
   if (i % 2 == 0)
   {
     lcd.setCursor(0, 1);
-    lcd.print("  Conectate al ");
+    lcd.print("WiFi IndoorMatic");
   }
   else
   {
     lcd.setCursor(0, 1);
-    lcd.print("WiFi IndoorMatic");
+    lcd.print("  Conectate al ");
   }
   delay(500);
 }
 
-// escribe enviando datoss en el LCD
+// escribe enviando datoss
 void lcdEnviandoDatos()
 {
   if( debug > 1)
@@ -459,14 +458,14 @@ void lcdBienvenida()
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("BIENVENIDO");
-  lcd.print(i);
+  //lcd.print(i);
   lcd.setCursor(0, 1);
   lcd.print("Num. Serie: ");
   lcd.print(numeroSerie);
   delay(2500);
 }
 
-//escibe en la pantalla datos recibidos
+//escibe datos recibidos en la pantalla
 void lcdExito()
 {
   if( debug > 1)
@@ -479,18 +478,35 @@ void lcdExito()
   lcd.setCursor(0, 0);
   lcd.print("   D a t o s    ");
   lcd.setCursor(0, 1);
-  lcd.print("Recibidos  O.K.");
+  lcd.print("Enviados  O.K.");
   delay(1000);
 }
 
-//escibe ONLINE en la pantalla datos recibidos
+//escibe fallo de conexion en la pantalla
+void lcdFracaso()
+{
+  if( debug > 1)
+  {
+    delay(1000);
+    Serial.print("\n");
+    Serial.println(F("lcdFracaso delay 1000ms"));
+  }
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("c o n e x i o n ");
+  lcd.setCursor(0, 1);
+  lcd.print(" f a l l i d a ");
+  delay(1000);
+}
+
+//escibe ONLINE en la pantalla
 void lcdONLINE()
 {
   if( debug > 1)
   {
     delay(1000);
     Serial.print("\n");
-    Serial.println(F("lcdOnline delay 1000ms"));
+    Serial.println(F("lcdOnline delay 3000ms"));
   }
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -500,6 +516,24 @@ void lcdONLINE()
   delay(3000);
 }
 
+//escibe ONLINE en la pantalla
+void lcdErrorLeeSensores()
+{
+  if( debug > 1)
+  {
+    delay(1000);
+    Serial.print("\n");
+    Serial.println(F("lcdErrorLeeSensores delay 1000ms"));
+  }
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(" - E R R O R -  ");
+  lcd.setCursor(0, 1);
+  lcd.print("S E N S O R E S ");
+  delay(3000);
+}
+
+
 /***********************************
  * F U N C I O N E S   S E R I A L *
 /**********************************/
@@ -508,8 +542,9 @@ void escuchaSerial()
   if (Serial.available())
   {
     stringDelSerial = Serial.readString();
+    stringCompleta = true;
   }
-  stringCompleta = true;
+
   if(debug > 0)
   {
     Serial.print("\n");
@@ -521,19 +556,38 @@ void escuchaSerial()
 void analizaComando( String comando )
 {
   comando.trim();
+
   if (stringCompleta)
   {
     stringCompleta = false;
+
     if (comando.equals("CONECTADO_OK"))
     {
       online = true;
+      esperandoRespuesta = false;
       lcdONLINE();
     }
-    if (comando.equals("EXITO"))
+
+    else if (comando.equals("EXITO"))
     {
       exito = true;
+      esperandoRespuesta = false;
+      dbg = 0;
+      lcdExito();
     }
 
+    else if (comando.equals("conexion_FALLIDA"))
+    {
+      exito = false;
+      esperandoRespuesta = false;
+      dbg += 1;
+      lcdFracaso();
+    }
+
+    else
+    {
+      esperandoRespuesta = false;
+    }
   }
   stringDelSerial = "";
 }
